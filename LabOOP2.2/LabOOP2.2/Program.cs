2e142c904fe6c09ede61862ccc3f57e5792d6c6a
MGromov <maksim_gromov_2012@mail.ru>
2016-04-11 20:55:45 +0300

    main

--- LabOOP2.2/LabOOP2.2/Program.cs
+++ LabOOP2.2/LabOOP2.2/Program.cs
@@ -10,6 +10,12 @@ namespace LabOOP2._2
     {
         static void Main(string[] args)
         {
+            Point A = new Point(0, 0);
+            Point B = new Point(0, 5);
+            Point C = new Point(5, 0);
+
+            Threeangle T = new Threeangle(A, B, C);
+            T.Show();
         }
     }
 }
