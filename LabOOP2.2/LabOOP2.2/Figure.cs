5671228be77e1e537a8e02d7c82f47e76b256e2f
MGromov <maksim_gromov_2012@mail.ru>
2016-04-11 20:08:41 +0300

    class Figure

---
+++ LabOOP2.2/LabOOP2.2/Figure.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabOOP2._2
+{
+    class Figure
+    {
+        public readonly List<Point> allPoints;
+
+        public Figure(Point A, Point B, Point C)
+        {
+            allPoints = new List<Point>();
+            allPoints.Add(A);
+            allPoints.Add(B);
+            allPoints.Add(C);
+        }
+
+        public Figure(Point A, Point B, Point C, Point D)
+        {
+            allPoints = new List<Point>();
+            allPoints.Add(A);
+            allPoints.Add(B);
+            allPoints.Add(C);
+            allPoints.Add(D);
+        }
+
+        public Figure(Point A, Point B, Point C, Point D, Point E)
+        {
+            allPoints = new List<Point>();
+            allPoints.Add(A);
+            allPoints.Add(B);
+            allPoints.Add(C);
+            allPoints.Add(D);
+            allPoints.Add(E);
+        }
+
+        protected double Length(Point A, Point B)
+        {
+            return Math.Sqrt(Math.Pow((A.x - B.x), 2) + Math.Pow((A.y - B.y), 2)); ;
+        }
+    }
+}
