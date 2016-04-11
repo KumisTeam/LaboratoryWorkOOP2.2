81b7126baccab3c3af011021612cd082c288acb6
MGromov <maksim_gromov_2012@mail.ru>
2016-04-11 19:58:40 +0300

    class Point

---
+++ LabOOP2.2/LabOOP2.2/Point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabOOP2._2
+{
+    class Point
+    {
+        public readonly int x;
+        public readonly int y;
+
+        public Point(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+        }
+    }
+}
