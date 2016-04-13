cb0ed4802a37ab2c72b2275c2ece757778a46b82
MGromov <maksim_gromov_2012@mail.ru>
2016-04-13 12:03:48 +0300

    Little fix

--- LabOOP2.2/LabOOP2.2(c++)/LabOOP2.2(c++).cpp
+++ LabOOP2.2/LabOOP2.2(c++)/LabOOP2.2(c++).cpp
@@ -2,10 +2,22 @@
 //
 
 #include "stdafx.h"
+#include "Point.h"
+#include "Threeangle.h"
 
+#include <iostream>
 
 int main()
 {
+	Point A(2, 4);
+	Point B(5, 2);
+	Point C(3, 5);
+
+	Threeangle ABC(A, B, C);
+
+	cout << ABC.Perim() << endl;
+	cout << ABC.Square() << endl;
+
     return 0;
 }
 
